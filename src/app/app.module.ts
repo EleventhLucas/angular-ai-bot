ad2733e23c59d85afab83ad52b8dc18335e3a7ce
JazzyLucas <59397873+EleventhLucas@users.noreply.github.com>
2025-06-05 19:24:53 -0400

    fixed stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatBotComponent } from './components/chat-bot/chat-bot';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatBotComponent
   ],
   imports: [
     BrowserModule,
